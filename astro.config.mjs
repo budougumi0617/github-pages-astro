43ec605e5a7c761ab4b615b5c6e0688604416969
budougumi0617 <budougumi0617@gmail.com>
2024-02-23 14:10:54 +0900

    feat: add github pages setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://budougumi0617.github.io',
+    base: '/github-pages-astro',
+});
